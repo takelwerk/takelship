cfa343b7630c38542b18915a0096f3c1aa55d894
Stephan <stephan@takelwerk.net>
2024-10-07 03:11:23 +0200

    Satisfy linter

--- ansible/molecule/registry/tests/test_registry_envvars.py
+++ ansible/molecule/registry/tests/test_registry_envvars.py
@@ -1,10 +1,9 @@
 import takeltest
-import json
 
 testinfra_hosts = [takeltest.hosts()[0]]
 
 
 def test_registry_envvars_service_disabled(host):
-    cmd_podman = f"pod podman ps -a -q -f name='docker-dind'"
+    cmd_podman = "pod podman ps -a -q -f name='docker-dind'"
 
     assert '' == host.check_output(cmd_podman)
