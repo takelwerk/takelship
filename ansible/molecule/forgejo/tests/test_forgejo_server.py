e3e1882d813e67cc6c083c33b399bf9c680dad8c
Stephan <stephan@takelwerk.net>
2024-10-08 06:09:21 +0200

    Add fortea

--- ansible/molecule/forgejo/tests/test_forgejo_server.py
+++ ansible/molecule/forgejo/tests/test_forgejo_server.py
@@ -1,5 +1,6 @@
 import takeltest
 import pytest
+import json
 from time import sleep
 
 testinfra_hosts = [takeltest.hosts()[0]]
@@ -30,3 +31,28 @@ def test_forgejo_server_setup_page(host, testvars):
 
     assert expected in curl_result
     assert unexpected not in curl_result
+
+
+@pytest.mark.forgejo
+def test_forgejo_server_setup_page(host, testvars):
+    port = testvars['takel_ship_forgejo_server_http_port']['port']
+    cmd = testvars['takel_ship_scripts_script_pod']['name']
+
+    for _ in range(30):
+        cmd_curl_server = (
+            f"{cmd} curl "
+            "--insecure "
+            f"http://localhost:{port}/api/v1/version")
+        curl_run = host.run(cmd_curl_server)
+        if curl_run.exit_status == 0:
+            break
+        sleep(1)
+
+    cmd_curl_server = (
+        f"{cmd} curl "
+        "--insecure "
+        f"http://localhost:{port}/api/v1/version")
+    curl_result = host.check_output(cmd_curl_server)
+    result = json.loads(curl_result)
+
+    assert 'version' in result
--- ansible/roles/takel_ship_forgejo/molecule/default/tests/test_takel_ship_forgejo_install.py
+++ ansible/roles/takel_ship_forgejo/molecule/default/tests/test_takel_ship_forgejo_install.py
@@ -11,3 +11,14 @@ def test_takel_ship_podman_install_packages_installed(host, testvars):
         deb = host.package(package)
 
         assert deb.is_installed
+
+
+def test_takel_ship_podman_create_bat_symlink(host):
+    tea = host.file('/usr/local/bin/tea')
+
+    assert tea.exists
+    assert tea.is_symlink
+    assert tea.linked_to == '/usr/bin/tea-cli'
+    assert tea.user == 'root'
+    assert tea.group == 'root'
+    assert tea.mode == 0o755
