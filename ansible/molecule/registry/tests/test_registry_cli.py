95943593f1525228395641773e84735abbbbbcc4
Stephan <stephan@takelwerk.net>
2024-10-09 23:48:30 +0200

    Fix takelship info yaml

--- ansible/molecule/registry/tests/test_registry_cli.py
+++ ansible/molecule/registry/tests/test_registry_cli.py
@@ -1,4 +1,5 @@
 import takeltest
+import yaml
 
 testinfra_hosts = [takeltest.hosts()[0]]
 
@@ -25,6 +26,16 @@ def test_registry_cli_update(testvars, docker_client):
     assert "[takelship] This is a takelwerk takelship container" in result
     assert "[takelship] Preparing podman container environment" in result
     assert "[takelship] Updating data directory" in result
+
+
+def test_registry_cli_info(testvars, docker_client):
+    image_user = testvars['project']['local_user']
+    image_repo = f"{testvars['project']['name']}-project"
+    image = f"{image_user}/{image_repo}:latest"
+    result = docker_client.containers.run(
+        image, "info").decode("utf-8")
+    takelshipinfo = yaml.safe_load(result)
+    assert 'name' in takelshipinfo
 
 
 def test_registry_cli_root(host, docker_client):
