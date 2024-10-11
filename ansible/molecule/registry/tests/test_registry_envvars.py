637a61912c23459e263fe2869d80d16545061657
Stephan <stephan@takelwerk.net>
2024-10-11 04:19:00 +0200

    Add tea config

--- ansible/molecule/registry/tests/test_registry_envvars.py
+++
@@ -1,9 +0,0 @@
-import takeltest
-
-testinfra_hosts = [takeltest.hosts()[0]]
-
-
-def test_registry_envvars_service_disabled(host):
-    cmd_podman = "pod podman ps -a -q -f name='docker-dind'"
-
-    assert '' == host.check_output(cmd_podman)
