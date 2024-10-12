e482c7befff751143b4d4e210ddebf4809341563
Stephan <stephan@takelwerk.net>
2024-10-13 01:31:33 +0200

    Add apt proxy

--- ansible/roles/takel_ship_compose/molecule/default/tests/test_takel_ship_compose_services.py
+++ ansible/roles/takel_ship_compose/molecule/default/tests/test_takel_ship_compose_services.py
@@ -174,3 +174,28 @@ def test_takel_ship_compose_compose_services_run_preinstall_files(
         assert file.user == user
         assert file.group == group
         assert file.mode == mode
+
+
+def test_takel_ship_compose_compose_services_dockerfile_files(
+        host, testvars):
+    user = testvars['takel_ship_podman_user']['owner']
+    group = testvars['takel_ship_podman_user']['group']
+    mode = int(testvars['takel_ship_podman_user']['mode']['file'], 8)
+    home_dir = testvars['takel_ship_compose_home_dir']
+    dist_dir = testvars['takel_ship_compose_dist_dir']
+    compose_dir = testvars['takel_ship_compose_compose_dir']
+    dockerfile = testvars['takel_ship_compose_dockerfile']
+    services = testvars['takel_ship_compose_services']
+    for service in services:
+        if 'dockerfile' not in service:
+            continue
+
+        file = host.file(
+            f"{home_dir}/{dist_dir}/{compose_dir}/"
+            f"{service['name']}/{dockerfile}")
+
+        assert file.exists
+        assert file.is_file
+        assert file.user == user
+        assert file.group == group
+        assert file.mode == mode
