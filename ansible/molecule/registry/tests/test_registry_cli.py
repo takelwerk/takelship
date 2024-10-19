4a5f85a3d022e782dd4267e2ff9d4a6f4cb9120c
Stephan <stephan@takelwerk.net>
2024-10-19 17:32:59 +0200

    Refactor project dump

--- ansible/molecule/registry/tests/test_registry_cli.py
+++ ansible/molecule/registry/tests/test_registry_cli.py
@@ -16,18 +16,6 @@ def test_registry_cli_no_argument(testvars, docker_client):
     assert "docker run --rm --interactive --tty --name takelship" in result
 
 
-def test_registry_cli_update(testvars, docker_client):
-    image_user = testvars['project']['local_user']
-    image_repo = f"{testvars['project']['name']}-project"
-    image = f"{image_user}/{image_repo}:latest"
-    result = docker_client.containers.run(
-        image, "dump").decode("utf-8")
-
-    assert "[takelship] This is a takelwerk takelship container" in result
-    assert "[takelship] Preparing podman container environment" in result
-    assert "[takelship] Updating data directory" in result
-
-
 def test_registry_cli_info(testvars, docker_client):
     image_user = testvars['project']['local_user']
     image_repo = f"{testvars['project']['name']}-project"
